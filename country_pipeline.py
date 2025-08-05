1f9d9a5393aee5104b2e2f5ee0aad667125f72e4
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-08-05 23:59:29 +0530

    making a small data pipeline.

--- country_pipeline.py
+++ country_pipeline.py
@@ -1,5 +1,19 @@
 #A simple data pipeline based on the country_full data set
 import pandas as pd
 
-def extract(file_path : str):
-    raw_data = pd.read_csv("./Data sets/country_full.csv")
+def extract(file_path : str) -> str:
+    raw_data = pd.read_csv(file_path)
+    return raw_data
+
+#transforming to clean the data
+def transform(raw_data):
+    empty_data = raw_data.loc[raw_data["intermediate-region"].isnull() , : ]
+    return empty_data
+
+#load the cleaned data in a csv file
+def load():
+    pass
+
+raw_data = extract("./Data sets/country_full.csv")
+
+print(transform(raw_data))
