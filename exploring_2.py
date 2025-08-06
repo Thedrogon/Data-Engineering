7b3c4f2d460aa78ac3016e192ff9bb2055d65f4e
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-08-06 22:56:28 +0530

    exploring json data

---
+++ exploring_2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def extract(filepath):
+    #raw_data = pd.read_json(filepath , orient='columns') bad way of extraction due to nested dicts
+    
+   pass
+
+
+extract("./Data sets/example_2.json")
